5c97b71bbaad915036067ee976a44086415c9f48
Spallasss <spallas@Spallas.localdomain>
2021-11-17 16:45:50 +0800

    HashMap

--- lib.rs
+++ lib.rs
@@ -1,13 +1,14 @@
+
 #![cfg_attr(not(feature = "std"), no_std)]
 
 use ink_lang as ink;
 
 #[ink::contract]
 mod incrementer {
-
     #[ink(storage)]
     pub struct Incrementer {
         value: i32,
+        my_value: ink_storage::collections::HashMap<AccountId, i32>,
     }
 
     impl Incrementer {
@@ -15,6 +16,7 @@ mod incrementer {
         pub fn new(init_value: i32) -> Self {
             Self {
                 value: init_value,
+                my_value: ink_storage::collections::HashMap::new(),
             }
         }
 
@@ -22,23 +24,71 @@ mod incrementer {
         pub fn default() -> Self {
             Self {
                 value: 0,
+                my_value: Default::default(),
             }
         }
 
         #[ink(message)]
-        pub fn get(&self) {
-            // Contract Message
+        pub fn get(&self) -> i32 {
+            self.value
+        }
+
+        #[ink(message)]
+        pub fn inc(&mut self, by: i32) {
+            self.value += by;
+        }
+
+        #[ink(message)]
+        pub fn get_mine(&self) -> i32 {
+            self.my_value_or_zero(&self.env().caller())
+        }
+
+        #[ink(message)]
+        pub fn inc_mine(&mut self, by: i32) {
+            // ACTION: Get the `caller` of this function.
+            // ACTION: Get `my_value` that belongs to `caller` by using `my_value_or_zero`.
+            // ACTION: Insert the incremented `value` back into the mapping.
+            let caller = self.env().caller();
+            let my_value = self.my_value_or_zero(&caller);
+            self.my_value.insert(caller, my_value + by);
+        }
+
+        fn my_value_or_zero(&self, of: &AccountId) -> i32 {
+            *self.my_value.get(of).unwrap_or(&0)
         }
     }
 
     #[cfg(test)]
     mod tests {
         use super::*;
+
         use ink_lang as ink;
 
         #[ink::test]
         fn default_works() {
-            Incrementer::default();
+            let contract = Incrementer::default();
+            assert_eq!(contract.get(), 0);
+        }
+
+        #[ink::test]
+        fn it_works() {
+            let mut contract = Incrementer::new(42);
+            assert_eq!(contract.get(), 42);
+            contract.inc(5);
+            assert_eq!(contract.get(), 47);
+            contract.inc(-50);
+            assert_eq!(contract.get(), -3);
+        }
+
+        #[ink::test]
+        fn my_value_works() {
+            let mut contract = Incrementer::new(11);
+            assert_eq!(contract.get(), 11);
+            assert_eq!(contract.get_mine(), 0);
+            contract.inc_mine(5);
+            assert_eq!(contract.get_mine(), 5);
+            contract.inc_mine(10);
+            assert_eq!(contract.get_mine(), 15);
         }
     }
 }
